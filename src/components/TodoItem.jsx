d50ce69acdad86ee3688562e14b49cb620332c44
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-17 14:36:24 +0800

    manage remoce

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useTodo } from '../lib/TodoContext'
 
 const TodoItem = ({item}) => {
+  const {dispatch} = useTodo()
+  const handleClick = () => {
+    dispatch({type:'REMOVE_TODO', payload:{id:item.id} })
+  }
   return (
     <div>
-        <p>{item.name} <button>Delete</button></p>
+        <p>{item.name} 
+        <button onClick={handleClick}>Delete</button></p>
     </div>
   )
 }
